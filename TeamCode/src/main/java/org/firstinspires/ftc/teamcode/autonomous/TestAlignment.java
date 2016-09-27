be21b5db5af7cbe721817e871ee24156fe4baeb8
Davis Haupt <dbh937@gmail.com>
2016-09-27 18:19:40 -0400

    finished refactor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/TestAlignment.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.teamcode.sensors.Vuforia;
+
+
+public class TestAlignment extends LinearOpMode{
+  public void runOpMode() throws InterruptedException {
+    Vuforia vuforia = new Vuforia();
+    waitForStart();
+    vuforia.activate();
+
+    while (opModeIsActive()) {
+      OpenGLMatrix loc = vuforia.getAlignment("gears");
+      float[] pos = vuforia.getPosition(loc);
+      float heading = vuforia.getHeading(loc); // heading is rotation around (y) axis.
+
+      telemetry.addData("Alignment", pos[0]);
+      telemetry.addData("Orientation", heading);
+      telemetry.addData("Distance", pos[2]);
+    }
+  }
+}
