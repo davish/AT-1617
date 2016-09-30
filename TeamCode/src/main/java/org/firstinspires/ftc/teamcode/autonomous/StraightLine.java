9fb544102ef9d6efb8ca31627b2e29522ef7ccfc
Davis Haupt <dbh937@gmail.com>
2016-09-30 13:54:40 -0400

    more autonomous stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/StraightLine.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/StraightLine.java
@@ -4,12 +4,14 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
-import org.firstinspires.ftc.teamcode.FtcUtil;
 import org.firstinspires.ftc.teamcode.chassis.*;
 import org.firstinspires.ftc.teamcode.sensors.Vuforia;
 
-@Autonomous(name="Align with Gears", group="Tests")
-public class GearAlign extends LinearOpMode {
+/**
+ * Created by davis on 9/29/16.
+ */
+@Autonomous(name="Line to Gears", group="Tests")
+public class StraightLine extends LinearOpMode{
   Holonomic robot;
 
   double SPEED = .15;
@@ -18,39 +20,25 @@ public class GearAlign extends LinearOpMode {
   public void runOpMode() throws InterruptedException{
     robot = new Omni();
     robot.init(hardwareMap);
-
     Vuforia vuforia = new Vuforia();
     waitForStart();
     vuforia.activate();
-
     while (opModeIsActive()) {
       OpenGLMatrix loc = vuforia.getAlignment(TARGET);
-      // pos={x, y z}. x is alignment, z is distance from target. y is height, which doesn't matter as of now.
+      // pos={x, y z}. y is alignment, z is distance from target. x is height, which doesn't matter as of now.
       float[] pos = vuforia.getPosition(loc);
       float heading = vuforia.getHeading(loc); // heading is rotation around (y) axis.
 
       double pow = 0, angle = 0, rot = 0;
 
-//      if (Math.abs(heading) > 14) {
-//        pow = 0;
-//        telemetry.addData("status", "aligning");
-//      }
-//      else
-      if (Math.abs(pos[1]) > 50) {
+      if (Math.abs(pos[2]) > 500) {
+        angle = Math.atan2(pos[2], pos[1]) + Math.PI/2;
         pow = SPEED;
-        angle = FtcUtil.sign(pos[1]) * -Math.PI/2;
-        telemetry.addData("status", "strafing");
-      }
-      else if (Math.abs(pos[2]) > 500) {
+      } else {
+        pow = 0;
         angle = 0;
-        pow = SPEED;
-        telemetry.addData("status", "approaching");
       }
 
-      telemetry.addData("power", pow);
-      telemetry.addData("angle", Math.toDegrees(angle));
-      telemetry.addData("rotation", rot);
-      telemetry.update();
 
       if (!Float.isNaN(heading))
         robot.moveStraight(pow, angle, heading);
